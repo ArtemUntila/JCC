c8a593899e37e1e75b1982d1048c3dcd2397241a
Artyom-IWT <untila_artem@mail.ru>
2021-05-31 17:00:12 +0300

    working with real tests generated by kex

--- src/main/java/jcc/CompilerTest.java
+++ src/main/java/jcc/CompilerTest.java
@@ -3,9 +3,8 @@ package jcc;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -21,29 +20,38 @@ public class CompilerTest {
         String pack = path.replaceAll("([A-Za-z0-9_]+)\\.java", ""); // путь к пакету
         System.out.println("pack = " + pack);
 
-        String name = path.replace(pack, "").replace(".java", "") + "Runnable"; // имя нового Runnable-класса
-        String namePath = pack + name + ".java"; // путь к новому Runnable-классу
+        String name = path.replace(pack, "").replace(".java", "") + "_Runnable"; // имя нового Runnable-класса
+        String namePath = "src/main/java/tests/" + name + ".java"; // путь к новому Runnable-классу
         System.out.println("name = " + name);
 
         BufferedWriter bW = Files.newBufferedWriter(Path.of(namePath));
 
         List<String> methods = new ArrayList<>(); // список с методами класса, которые нужно запустить в run()
-        int lastIndex = lines.size() - 1; // последняя строка - закрывающаяся фигурная скобка
-        for (int i = 0; i < lastIndex; i++) {
+        int braceCounter = 0;
+        boolean insideClass = false;
+        for (int i = 0; i < lines.size() - 1; i++) {
             String line = lines.get(i);
+            if (line.contains("package")) {
+                line = "package tests;";
+            }
             if (line.contains("class")) { // меняем имя класса и ставим реализацию Runnable
                 line = line.replaceAll("class [A-Za-z0-9_]+", "class " + name).
                         replace("{", "implements Runnable {");
+                insideClass = true;
             }
-            if (line.contains("public void") && lines.get(i - 1).contains("@Test")) { // добавляем метод, необходимый для запуска
-                methods.add(line.split("(\\s+public void\\s+)|(\\s)")[1]);
+            if (line.contains("public  void") && lines.get(i - 1).contains("@Test")) { // добавляем метод, необходимый для запуска
+                methods.add(line.split("(\\s+public  void\\s+)|(\\s)")[1]);
             }
+            if (line.contains("{"))
+                braceCounter++;
+            else if(line.contains("}"))
+                braceCounter--;
+            if (braceCounter == 0 && insideClass) break;
             bW.write(line);
             bW.newLine();
         }
 
         bW.write(addRunMethod(methods)); // добавление метода run()
-        bW.write(lines.get(lastIndex));
         bW.close();
 
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
@@ -52,20 +60,36 @@ public class CompilerTest {
 
     public static void main(String[] args) throws Exception {
         CompilerTest cT = new CompilerTest();
-        cT.compileToRunnable("src/main/java/jcc/Adder.java");
-        cT.compileToRunnable("src/main/java/tests/TestAdder.java");
-        ClassLoader cL = new URLClassLoader(new URL[]{new URL("file:src/main/java/tests/TestAdderRunnable.class")});
+        File dir = new File("tests/jcc");
+        if (dir.isDirectory()) {
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                cT.compileToRunnable(file.getAbsolutePath());
+            }
+        }
+        /*cT.compileToRunnable("src/main/java/tests/TestAdder.java");
+        cT.compileToRunnable("src/main/java/tests/Adder.java");
+        File file = new File("src/main/java/tests/TestAdderRunnable.class");
+        System.out.println(file.exists());
+        ClassLoader cL = new URLClassLoader(new URL[]{new URL("file:src/main/java/tests")});
         Class<?> klass = cL.loadClass("tests.TestAdderRunnable");
+        InputStream original = cL.getResourceAsStream("tests/TestAdderRunnable.class");
+        System.out.println(Arrays.toString(original.readAllBytes()));
         Runnable targetInstance = (Runnable) klass.getDeclaredConstructor().newInstance();
-        targetInstance.run();
+        targetInstance.run();*/
     }
 
     private String addRunMethod(List<String> methods) { // добавление метода run()
-        StringBuilder run = new StringBuilder("\n    @Override\n    public void run() {");
+        StringBuilder run = new StringBuilder("\n    @Override\n    public void run() {" +
+                "\n        try {");
         for (String method : methods) {
             run.append("\n        ").append(method).append(";");
         }
-        run.append("\n    }\n");
+        run.append("\n        } catch (Throwable throwable) {" +
+                "\n            throwable.printStackTrace();" +
+                "\n        }" +
+                "\n    }" +
+                "\n\n}");
         return run.toString();
     }
 }
